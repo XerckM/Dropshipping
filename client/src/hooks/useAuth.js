b02e3ad0c03a11caca0c2c2ceb46489b4919a6be
Xerckiem <mercadoxerckiem@gmail.com>
2023-12-02 00:30:07 -0500

    minor changes to authentication

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react';
 
 const useAuth = () => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); // Add a loading state
 
   useEffect(() => {
     const storedUser = JSON.parse(localStorage.getItem('user'));
     if (storedUser) {
       setUser(storedUser);
     }
+    setLoading(false); // Set loading to false after user state is initialized
   }, []);
 
   const login = (userData) => {
@@ -20,7 +22,7 @@ const useAuth = () => {
     setUser(null);
   };
 
-  return { user, login, logout };
+  return { user, loading, login, logout }; // Include loading in the return value
 };
 
 export default useAuth;
--- client/src/utils/ProtectedRoute.js
+++ client/src/utils/ProtectedRoute.js
@@ -3,8 +3,12 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { useAuthState } from '../store/authContext'; // Adjust the import path as needed
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAuthState();
+  const { user, loading } = useAuthState(); // Destructure loading from the context
   const location = useLocation();
+
+  if (loading) {
+    return <div>Loading...</div>; // Render loading indicator or null while loading
+  }
 
   if (!user) {
     return <Navigate to="/auth" state={{ from: location }} replace />;
